b744833309f5b23c8db24d74fa293d5c3d77a1cb
kok i <bluewhitered123@gmail.com>
2020-10-05 21:17:50 +0900

    [fix]julia2
    julia2

--- static/shaders/Julia.fs
+++ static/shaders/Julia.fs
@@ -9,10 +9,10 @@ vec3 hsv(float h, float s, float v){
 }
 
 void main(void){
-    vec2 pos = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y)*0.01;
+    vec2 pos = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
     vec2 p = vec2(pos.x-0.505,pos.y+0.1);
     
-    float costam = 39000.0;
+    float costam = 3000.0;
     int j = 0;
     vec2 x = vec2(-0.345, 0.654);
     vec2 y = vec2(costam * 0.000005, 0.0);
@@ -23,7 +23,7 @@ void main(void){
         z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + x + y;
     }
 
-    float costam2 = 80000.0;
+    float costam2 = 8000.0;
     float h = abs(mod(costam2 * 0.001 - float(j), 360.0) / 360.0);;
     vec3 rgb = vec3(0,h,0);//hsv(h, 1.0, 1.0);
     gl_FragColor = vec4(rgb, 1.0);
