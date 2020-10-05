67de84ee372bd6e7914a4ef808c6de05cc9b42ef
kok i <bluewhitered123@gmail.com>
2020-10-05 20:56:19 +0900

    [fix] ios shead error
    costom time chage time

--- static/shaders/Julia.fs
+++ static/shaders/Julia.fs
@@ -12,10 +12,10 @@ void main(void){
     vec2 pos = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y)*0.2;
     vec2 p = vec2(pos.x-0.5,pos.y+0.1);
     
-    float costam = 39000.0;
+    //float costam = 39000.0;
     int j = 0;
     vec2 x = vec2(-0.345, 0.654);
-    vec2 y = vec2(costam * 0.000005, 0.0);
+    vec2 y = vec2(time * 0.000001, 0.0);
     vec2 z = p;
     for(int i = 0; i < 360; i++){
         j++;
@@ -23,8 +23,8 @@ void main(void){
         z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + x + y;
     }
 
-    float costam2 = 80000.0;
-    float h = abs(mod(costam2 * 0.001 - float(j), 360.0) / 360.0);;
+    //float costam2 = 80000.0;
+    float h = abs(mod(time * 0.001 - float(j), 360.0) / 360.0);;
     vec3 rgb = vec3(0,h,0);//hsv(h, 1.0, 1.0);
     gl_FragColor = vec4(rgb, 1.0);
     
